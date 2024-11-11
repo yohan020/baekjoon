01f447ca948714487ed6b1d41af76fd62eb5959d
yohan <yohanlee10@naver.com>
2024-11-11 23:16:13 +0900

    2579

---
+++ baekjoon/2579.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int DP[301]; // DP[i]는 i번째 계단까지 도달했을 때의 최대 점수
+int score[301];
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> score[i];
+    }
+
+    // 초기값 설정
+    DP[1] = score[1];
+    if (n >= 2) {
+        DP[2] = score[1] + score[2];
+    }
+
+    // DP 점화식 적용
+    for (int i = 3; i <= n; i++) {
+        DP[i] = max(DP[i - 2] + score[i], DP[i - 3] + score[i - 1] + score[i]);
+    }
+
+    cout << DP[n] << endl;
+
+    return 0;
+}
--- baekjoon/9095.cpp
+++ baekjoon/9095.cpp
@@ -5,17 +5,17 @@ using namespace std;
 int DP[12];
 
 int main() {
-
+	
 	int n, T;
 	cin >> n;
 	DP[1] = 1;
 	DP[2] = 2;
 	DP[3] = 4;
 	for (int i = 4; i < 12; i++) DP[i] = DP[i - 1] + DP[i - 2] + DP[i - 3];
-
+	
 	for (int i = 0; i < n; i++) {
 		cin >> T;
 		cout << DP[T] << endl;
 	}
-
+	
 }
