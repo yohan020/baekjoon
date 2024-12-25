b9f6e9f03a1e22b37b67c3c86244f1b2d4405d23
yohan <yohanlee10@naver.com>
2024-12-25 22:36:00 +0900

    1080번 시간 오버로 아직 해결안함

--- baekjoon/1080.cpp
+++ baekjoon/1080.cpp
@@ -3,35 +3,28 @@
 
 using namespace std;
 
-// 행렬은 같은 곳 두번 뒤집으면 의미 없음!
-
 void reverse(vector<string> &A, int x, int y) {
   for (int i = x; i < x + 3; i++) {
     for (int j = y; j < y + 3; j++) {
-      if (A[i][j] == '0') A[i][j] = '1';
-      else A[i][j] = '0';
+      A[i][j] = (A[i][j] == '0') ? '1' : '0';
     }
   }
 }
-int N, M, count;
 
 int main() {
-  count = 0;
-  string temp;
+  int N, M, count = 0;
   cin >> N >> M;
-  vector<string> A;
-  vector<string> B;
+  vector<string> A(N), B(N);
+
   for (int i = 0; i < N; i++) {
-    cin >> temp;
-    A.push_back(temp);
+    cin >> A[i];
   }
   for (int i = 0; i < N; i++) {
-    cin >> temp;
-    B.push_back(temp);
+    cin >> B[i];
   }
 
-  for (int i = 0; i < N-2; i++) {
-    for (int j = 0; j < M-2; j++) {
+  for (int i = 0; i < N - 2; i++) {
+    for (int j = 0; j < M - 2; j++) {
       if (A[i][j] != B[i][j]) {
         reverse(A, i, j);
         count++;
@@ -40,17 +33,13 @@ int main() {
   }
 
   bool flag = true;
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < M; j++) {
+  for (int i = 0; i < N && flag; i++) {
+    for (int j = 0; j < M && flag; j++) {
       if (A[i][j] != B[i][j]) {
         flag = false;
       }
     }
   }
-  if (flag) {
-    cout << count << endl;
-  }
-  else {
-    cout << -1 << endl;
-  }
+
+  cout << (flag ? count : -1) << endl;
 }
