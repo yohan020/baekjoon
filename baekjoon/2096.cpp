4ddd020de8eff596da350c264ef47947b1e1d96a
YOHAN LEE <yohanlee10@naver.com>
2025-02-23 14:26:32 +0900

    2096

--- baekjoon/2096.cpp
+++ baekjoon/2096.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <cstring>
 #include <vector>
 
 using namespace std;
@@ -9,12 +10,13 @@ int main(){
     cin.tie(NULL);
 
     int N;
-    int min_result = 987654321;
-    int max_result = 0;
-    int dp[3][2] = {{0, 0}, {0, 0}, {0, 0}}; // 0-min 1-max , 굳이 다 저장할 필요는 없다 n과 n-1위치만 남기면됨
-    int temp[3][2] = {{0, 0}, {0, 0}, {0, 0}}; // n-1부분
+    int dp[3][2];
+    int temp[3][2];
     int n[100000][3];
     cin >> N;
+
+    memset(dp, 0,sizeof(dp));
+    memset(temp, 0,sizeof(temp));
 
     for (int i = 1; i <= N; i++) {
         cin >> n[i][0] >> n[i][1] >> n[i][2];
@@ -29,14 +31,11 @@ int main(){
         dp[2][0] = min(temp[1][0], temp[2][0]) + n[i][2];
         dp[2][1] = max(temp[1][1], temp[2][1]) + n[i][2];
 
-        temp[0][0] = dp[0][0];
-        temp[0][1] = dp[0][1];
-        temp[1][0] = dp[1][0];
-        temp[1][1] = dp[1][1];
-        temp[2][0] = dp[2][0];
-        temp[2][1] = dp[2][1];
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 2; k++) {
+                temp[j][k] = dp[j][k];
+            }
+        }
     }
-    min_result = min(dp[0][0], min(dp[1][0], dp[2][0]));
-    max_result = max(dp[0][1], max(dp[1][1], dp[2][1]));
-    cout << max_result << " " << min_result;
+    cout << max(dp[0][1], max(dp[1][1], dp[2][1])) << " " << min(dp[0][0], min(dp[1][0], dp[2][0]));
 }
