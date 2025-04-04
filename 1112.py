b853438fd80c5ee4089e666a9efbbe33b9b0ad88
YOHAN LEE <yohanlee10@naver.com>
2025-04-04 23:11:04 +0900

    1112

--- 1112.py
+++ 1112.py
@@ -1,37 +1,28 @@
 import sys
-import math as m
-input = sys.stdin.readline 
+input = sys.stdin.readline
 
-def solve1(x, b):
+def convert(x, b):
     if x == 0:
-        print(0)
-    
-    digits = []
-    temp = abs(b)
-    
-    while x != 0:
-        r = x % temp  
-        x = (x - r) // b  
-        digits.append(str(r))
-    
-    print("".join(reversed(digits)))
-        
-def solve2(x, b):
-    digits = []
-    temp = abs(x)
-    while temp > 0:
-        digits.append(str(temp % b))
-        temp //= b
-    digits.reverse()
-    if x < 0:
-        print("-"+"".join(digits))
-    elif x > 0:
-        print("".join(digits))
+        return "0"
+
+    if b > 0:
+        sign = ""
+        if x < 0:
+            sign = "-"
+            x = -x
+        digits = []
+        while x:
+            digits.append(str(x % b))
+            x //= b
+        return sign + "".join(reversed(digits))
     else:
-        print(0)
+        temp_b = -b
+        digits = []
+        while x != 0:
+            r = x % temp_b
+            x = (x - r) // b
+            digits.append(str(r))
+        return "".join(reversed(digits))
+
 x, b = map(int, input().split())
-if b < -1:
-    solve1(x, b)
-    
-elif b > 1:
-    solve2(x, b)
+sys.stdout.write(convert(x, b))
